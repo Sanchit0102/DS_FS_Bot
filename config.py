44b76e4363bb00ec23f3f55171325d3edb98553a
Sanchit0102 <137860157+Sanchit0102@users.noreply.github.com>
2023-07-08 20:58:12 +0530

    config.py

--- config.py
+++ config.py
@@ -62,7 +62,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(1562935405)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
